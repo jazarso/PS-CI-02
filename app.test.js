33735a30bcc6e61fb7edb51b28d96e919344639c
F. Javier Zarazaga-Soria <javy@unizar.es>
2023-02-21 11:15:05 +0100

    V2

--- app.test.js
+++ app.test.js
@@ -1,15 +1,17 @@
 const app = require("./app")
+const server = require("./server")
 const supertest = require("supertest")
+
 const request = supertest(app)
 
-describe("/ (root endpoint)", () => {
-    it("debería responder un hola mundo", async () => {
-        const response = await request.get("/")
-        expect(response.status).toBe(200)
-        expect(response.body.message).toBe("Hello Node World!");        
-    })
-}) 
+it("Debe responder un hola mundo", async () => {
+  const response = await request.get("/")
+  expect(response.status).toBe(200)
+  expect(response.body.message).toBe("Hola Mundo!")  
+})
 
-afterAll(done => {    
-    done();
+
+afterAll(() => {        
+  server.closeAll()  
 });
+
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-// server.js
 const app = require('./app')
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+server = app.listen(PORT, () => {
   console.log(`Servidor web en el puerto ${PORT}.`);
 });
+
+server.closeAll = () => {
+  app.con.end()  // Cierra conexión de BD
+  server.close()  // Cierra servidor web
+}
+
+module.exports = server
